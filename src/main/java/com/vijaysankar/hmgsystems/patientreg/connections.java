61482f895b67bed51e288292a55ae4a14a7108f3
Vijay S <vijay.sankar@global.chainsys.com>
2020-02-24 11:30:46 +0530

    Updated Connection

--- src/main/java/com/vijaysankar/hmgsystems/patientreg/connections.java
+++ src/main/java/com/vijaysankar/hmgsystems/patientreg/connections.java
@@ -2,14 +2,17 @@ package com.vijaysankar.hmgsystems.patientreg;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.TimeZone;
 
 import org.jdbi.v3.core.Jdbi;
 import org.jdbi.v3.sqlobject.SqlObjectPlugin;
 
 public class connections {
 	public static Connection TestConnections() throws Exception {
+		TimeZone timeZone = TimeZone.getTimeZone("Asia/Kolkata");
+		TimeZone.setDefault(timeZone);
 		Class.forName("oracle.jdbc.driver.OracleDriver");
-		Connection connection=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE","system","oracle");
+		Connection connection=DriverManager.getConnection("jdbc:oracle:thin:@13.235.147.120:1521:XE","vijay","vijay");
 		System.out.println(connection);
 		return connection;
 	}
